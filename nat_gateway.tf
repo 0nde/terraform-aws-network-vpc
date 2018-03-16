b7cd8af8ed1ded76fa7429a0687b815a6fdeecec
Fabien <git@joyon.fr>
2018-03-16 10:53:45 +0100

    adding tags

--- nat_gateway.tf
+++ nat_gateway.tf
@@ -1,10 +1,18 @@
 # nat gateway
 
 resource "aws_eip" "nat" {
-    vpc = true
+  vpc = true
+  tags {
+    Name    = "${var.resource_name}-nat-gw"
+    Billing = "${var.billing}"
+  }
 }
 
 resource "aws_nat_gateway" "default" {
   allocation_id = "${aws_eip.nat.id}"
   subnet_id     = "${aws_subnet.dmz.0.id}"
+  tags {
+    Name    = "${var.resource_name}-nat-gw"
+    Billing = "${var.billing}"
+  }
 }
